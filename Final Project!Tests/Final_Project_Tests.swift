53d3560b9dea037bde9b40651265dd28a4b494eb
Ella Sklar <esklar22@gmail.com>
2020-07-29 14:16:27 -0400

    Initial Commit

---
+++ Final Project!Tests/Final_Project_Tests.swift
@@ -0,0 +1,34 @@
+//
+//  Final_Project_Tests.swift
+//  Final Project!Tests
+//
+//  Created by Apple on 7/29/20.
+//  Copyright © 2020 Ella Sklar. All rights reserved.
+//
+
+import XCTest
+@testable import Final_Project_
+
+class Final_Project_Tests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
